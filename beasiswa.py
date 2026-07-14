95cdf199daa53b93cd006cdcd3711c9f2d5b2706
fauziahmaratu27-source <fauziahmaratu27@gmail.com>
2026-07-14 18:49:33 +0700

    beasiswa.py

--- beasiswa.py
+++ beasiswa.py
@@ -9,7 +9,7 @@ print("HASIL ANALISIS")
 Nilai_Akhir=((Nilai_UTS * 0.4)+(Nilai_UAS * 0.6))
 if not pernah_melanggar and Nilai_Akhir >=90 and Kehadiran>= 95:
 	print("Selamat! Anda lolos seleksi beasiswa Kategori A")
-if not pernah_melanggar and Nilai_Akhir>=80 and Kehadiran>=85:
+elif not pernah_melanggar and Nilai_Akhir>=80 and Kehadiran>=85:
 	print("Selamat! Anda lolos beasiswa Kategori B")
 else:
 	print("Mohon maaf Anda tidak lolos seleksi")
